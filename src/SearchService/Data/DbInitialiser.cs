f887dd28cf9b646e2b92bc89d2ea8654f897ca55
dion <dion.upton@p2dl.com>
2023-08-14 15:17:04 +0100

    Search Microservice

---
+++ src/SearchService/Data/DbInitialiser.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.Services;
+
+namespace SearchService.Data;
+
+public static class DbInitialiser
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb",
+            MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Colour, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+        
+        Console.WriteLine(items.Count + " returned from auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+
+    }
+}
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using SearchService.Data;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitialiser.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+});
+    
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
