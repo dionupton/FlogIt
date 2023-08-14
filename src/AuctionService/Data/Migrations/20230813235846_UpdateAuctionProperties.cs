4e7863d604aacc3ec1113effca6b24a6395f24c2
dion <dion.upton@p2dl.com>
2023-08-14 02:16:48 +0100

    Auction Microservice

---
+++ src/AuctionService/Data/Migrations/20230813235846_UpdateAuctionProperties.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAuctionProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AuctionEnded",
+                table: "Auctions",
+                newName: "AuctionEnd");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReservePrice",
+                table: "Auctions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservePrice",
+                table: "Auctions");
+
+            migrationBuilder.RenameColumn(
+                name: "AuctionEnd",
+                table: "Auctions",
+                newName: "AuctionEnded");
+        }
+    }
+}
