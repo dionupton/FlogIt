d9a5dbe1ab93f18150bf1cc04cb751af2efe87a5
dion <dion.upton@p2dl.com>
2023-09-05 17:00:35 +0100

    Gateway Service

---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,25 @@
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("---> Consuming Bid Placed");
+
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if(context.Message.BidStatus.Contains("Accepted") 
+            && context.Message.Amount > auction.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await auction.SaveAsync();
+            }
+        }
+
+    }
+}
