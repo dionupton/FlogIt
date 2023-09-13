9c525020fdd6aabb60a9f2dd58750e57fd9d3650
dion <60044681+dionupton@users.noreply.github.com>
2023-09-13 09:44:16 +0100

    isMobile check and py changes

--- frontend/webapp/app/page.tsx
+++ frontend/webapp/app/page.tsx
@@ -1,9 +1,12 @@
+import { isMobile } from "react-device-detect";
 import Listings from "./auctions/Listings";
 
 export default function Home() {
-  return (
-    <div>
+  <div>
+    {isMobile ? (
+      <h3>This website is not optimized for mobile devices.</h3>
+    ) : (
       <Listings />
-    </div>
-  )
+    )}
+  </div>
 }
